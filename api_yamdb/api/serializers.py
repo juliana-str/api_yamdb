48adc4fda7e9b2a8d7047590ae3d549938357c10
Юлиана Стрельникова <julianka.str@gmail.com>
2023-04-25 16:04:50 +0300

    serializer fix

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     """Сериалайзер для модели категория."""
-    name = serializers.SlugRelatedField(
+    category = serializers.SlugRelatedField(
         slug_field='name',
         read_only=True
     )
@@ -37,43 +37,41 @@ class GenreSerializer(serializers.ModelSerializer):
         queryset=Genre.objects.all()
     )
 
-    def validate_slug(self):
-        slug = re.match(r'^[-a-zA-Z0-9_]+$', self.genre)
-        if not slug:
-            raise serializers.ValidationError(
-                'Неверный слаг!')
-        return slug
+    def validate_slug(self, slug):
+        def validate_slug(self, slug):
+            if not re.match(r'^[-a-zA-Z0-9_]+$', slug):
+                raise serializers.ValidationError(
+                    'Неверный слаг!')
+            return slug
 
     class Meta:
-        read_only_fields = '__all__'
+        fields = '__all__'
         model = Genre
 
 
 class TitleSerializer(serializers.ModelSerializer):
     """Сериалайзер для модели произведения."""
     category = serializers.SlugRelatedField(
-        slug_field='title',
+        slug_field='category',
         read_only=True
     )
     genre = serializers.SlugRelatedField(
         slug_field='genre',
         read_only=True
     )
+    rating = serializers.IntegerField()
 
     def validate_year(self, data):
         """Проверка года выпуска произведения."""
         date = datetime.date()
-        if self.context.year > date:
+        if self.context.year > date.year:
             raise serializers.ValidationError(
                 'Произведение еще не вышло!')
         return data
 
-    def validate_rating(self, data):
-        if 0 < data > 10:
-            raise serializers.ValidationError(
-                'Поставьте оценку от 1 до 10!')
-        return data
+
 
     class Meta:
         fields = '__all__'
         model = Title
+
