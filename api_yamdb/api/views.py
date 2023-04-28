12ba512e063894f3187fd453d9b3f800fe165f9d
Юлиана Стрельникова <julianka.str@gmail.com>
2023-04-28 18:30:13 +0300

    serializer, pagination

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,11 +4,10 @@ from django.db import IntegrityError
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.viewsets import  ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters, status, permissions, serializers
 
 from .permissions import IsAdminOrReadOnly, IsAuthorOrAdminOrModerOnly
